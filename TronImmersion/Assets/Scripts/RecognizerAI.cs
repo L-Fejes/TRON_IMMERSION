1bd89f711d62f83801a1d9fee8fdcbb7d4b8ce79
Zach <zacharyfejes@gmail.com>
2017-08-06 16:29:15 -0400

    Added Recognizer to the game, including prefab and simple float-around AI to add some more population to the game and environment

---
+++ TronImmersion/Assets/Scripts/RecognizerAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecognizerAI : MonoBehaviour {
+
+    Vector3 movementZ;
+    float m_speed = 20.0f;
+    Rigidbody m_rb;
+
+    float m_flight_boundary = 500.0f;
+
+    private void Awake()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+	// Update is called once per frame
+	private void FixedUpdate () {
+        handleFlyForward();
+	}
+
+    void handleFlyForward()
+    {
+        movementZ = transform.forward * m_speed * Time.deltaTime;
+        m_rb.MovePosition(transform.position + movementZ);
+    }
+
+}
