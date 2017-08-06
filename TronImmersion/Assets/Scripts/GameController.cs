8962a3991f7c3398a3c7009bd5709df2c3091752
Lucas <l.j.b.fejes@gmail.com>
2017-08-06 14:23:11 -0400

    Another commit

--- TronImmersion/Assets/Scripts/GameController.cs
+++ TronImmersion/Assets/Scripts/GameController.cs
@@ -6,6 +6,8 @@ public class GameController : MonoBehaviour{
     
     public GameObject m_grid;
     public PlayerController m_player;
+    //public AIController m_challenger;
+
     public GameObject[] m_walls;
 
     public bool m_gameOver;
@@ -28,21 +30,6 @@ public class GameController : MonoBehaviour{
         }
     } */
 
-    public void AIKilled()
-    {
-        Debug.Log("Someone is dead");
-        
-        for(int i = 0; i < AIManager.allAI.Length; i++)
-        {
-            if(AIManager.allAI[i].activeSelf)
-            {
-                return;
-            }
-        }
-
-        Win();
-    }
-/*
     public void GameOver(bool win) {
         Debug.Log("Someone is dead");
         if (win) {
@@ -50,9 +37,8 @@ public class GameController : MonoBehaviour{
         } else {
             Lose();
         }
-
     }
-    */
+
     void Win() {
         Time.timeScale = 0.0f;
         SceneManager.LoadScene("Win", LoadSceneMode.Additive);
@@ -60,7 +46,7 @@ public class GameController : MonoBehaviour{
         m_gameOver = true;
     }
 
-    public void Lose() {
+    void Lose() {
         Time.timeScale = 0.0f;
         SceneManager.LoadScene("Lose", LoadSceneMode.Additive);
         Time.timeScale = 1.0f;
